ac0fc0f243da5f56aec4f08eee5096149b0a2f41
Muhammad Nauman Raza <devraza@outlook.com>
2022-03-29 17:43:28 +0100

    Install script (arch

---
+++ install.sh
@@ -0,0 +1,8 @@
+# sudo pacman -S zathura mpv dunst bspwm cava gpick neofetch micro polybar rofi sxhkd
+# yay -S spotify-adblock-git spicetify-cli manga-cli-git ani-cli
+
+git clone https://github.com/devraza/dotfiles
+cd dotfiles
+cp .config/* ~/.config/
+
+echo All done!
