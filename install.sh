ee0bf17dfe2a99e8c263ca8addb15a2cb19ccdde
Muhammad Nauman Raza <devraza@outlook.com>
2022-03-31 19:59:27 +0100

    Fix install script

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
-# sudo pacman -S zathura mpv dunst bspwm cava gpick neofetch micro polybar rofi sxhkd
-# yay -S spotify-adblock-git spicetify-cli manga-cli-git ani-cli
+sudo pacman -S zathura mpv dunst bspwm neofetch micro rofi sxhkd
+yay -S spotify-adblock-git spicetify-cli manga-cli-git ani-cli polybar eww cava  
 
 git clone https://github.com/devraza/dotfiles
 cd dotfiles
