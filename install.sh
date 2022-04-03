a4eb6e10a901cab46cb2bb130e9255748fb9c974
Muhammad Nauman Raza <devraza@outlook.com>
2022-04-03 08:34:07 +0100

    Fix

--- install.sh
+++ install.sh
@@ -1,8 +1,11 @@
-sudo pacman -S zathura mpv dunst bspwm neofetch micro rofi sxhkd
-yay -S spotify-adblock-git spicetify-cli manga-cli-git ani-cli polybar eww cava  
+sudo pacman -S zathura mpv dunst bspwm neofetch micro rofi sxhkd git
+yay -S spotify-adblock-git spicetify-cli manga-cli-git ani-cli polybar cava  
 
 git clone https://github.com/devraza/dotfiles
 cd dotfiles
-cp .config/* ~/.config/
+cp -r .config/* ~/.config/
+cp -r .local/* ~/.local/
+cp .Xresources ~/.Xresources
+cp .xinitrc ~/.xinitrc
 
-echo All done!
+echo "All done!"
